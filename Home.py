090a5db429ef9003c43b8d175bec5c8c575f3be3
Dimitris Patros <50484525+DimitrisPatros@users.noreply.github.com>
2023-06-09 10:24:56 +0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import pandas as pd
 
+
+st.set_page_config(
+    page_title="RA_France",
+    page_icon="🚗💥🚙",
+)
+
 """
 # Welcome to IC team!
 
@@ -11,4 +17,16 @@ from PIL import Image
 st.image("https://waterlust.com/cdn/shop/collections/SB_BG_016_1600x.jpg?v=1668631373",
             width=600 # Manually Adjust the width of the image as per requirement
         )
+
+st.write("# Welcome to Streamlit! 👋")
+
+st.sidebar.success("Select a demo above.")
+
+st.markdown(
+    """
+    # Welcome to IC team!
+
+    Bonsoir nia mouah !!!!! καλημέρα
+    """
+)
 
