6f76b507bab638178f5ab1be1b33467135e28094
Dimitris Patros <50484525+DimitrisPatros@users.noreply.github.com>
2023-06-09 12:46:18 +0300

    Update Data_Visualization.py

--- pages/Data_Visualization.py
+++ pages/Data_Visualization.py
@@ -5,10 +5,15 @@ import streamlit.components.v1 as components
 # >>> fig = px.box(range(10))
 # >>> fig.write_html('test.html')
 
+st.set_page_config(
+     page_title="Data visualization",
+     page_icon="🧊",
+     layout="wide",
+ )
 
-HtmlFile = open("output.html", 'r', encoding='utf-8')
-source_code = HtmlFile.read() 
-print(source_code)
+# HtmlFile = open("output.html", 'r', encoding='utf-8')
+# source_code = HtmlFile.read() 
+# print(source_code)
 #components.html(source_code, height = 700, width=800,scrolling =True)
-components.iframe("https://dionysisk.eu/RSA/report.html",height = 700, width=80%,scrolling =True)
+components.iframe("https://dionysisk.eu/RSA/report.html",height = 700, width=800,scrolling =True)
 
