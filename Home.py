c0ef0095ba074a658ff8def1231c98ff280c3991
Dimitris Patros <50484525+DimitrisPatros@users.noreply.github.com>
2023-06-09 14:20:30 +0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -14,8 +14,8 @@ Bonsoir nia mouah !!!!! καλημέρα
 
 """
 from PIL import Image
-st.image("https://waterlust.com/cdn/shop/collections/SB_BG_016_1600x.jpg?v=1668631373",
-            width=600 # Manually Adjust the width of the image as per requirement
+st.image("https://upload.wikimedia.org/wikipedia/commons/2/2f/Multi_vehicle_accident_-_M4_Motorway%2C_Sydney%2C_NSW_%288076208846%29.jpg",
+            width=700 # Manually Adjust the width of the image as per requirement
         )
 
 st.write("# Welcome to Streamlit! 👋")
