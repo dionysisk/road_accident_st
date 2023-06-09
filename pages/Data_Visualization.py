3cdf06533128a974212c40bc1efc3ef6996e9eaa
Dimitris Patros <50484525+DimitrisPatros@users.noreply.github.com>
2023-06-09 12:29:17 +0300

    Update Data_Visualization.py

--- pages/Data_Visualization.py
+++ pages/Data_Visualization.py
@@ -9,4 +9,6 @@ import streamlit.components.v1 as components
 HtmlFile = open("output.html", 'r', encoding='utf-8')
 source_code = HtmlFile.read() 
 print(source_code)
-components.html(source_code, height = 700, width=800,scrolling =True)
+#components.html(source_code, height = 700, width=800,scrolling =True)
+components.iframe(source_code, height = 700, width=800,scrolling =True)
+
