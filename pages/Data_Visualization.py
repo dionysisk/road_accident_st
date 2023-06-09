a4b291a1f39c4ff32f9906dddba0d25801cb8af0
Dimitris Patros <50484525+DimitrisPatros@users.noreply.github.com>
2023-06-09 12:51:02 +0300

    Update Data_Visualization.py

--- pages/Data_Visualization.py
+++ pages/Data_Visualization.py
@@ -16,5 +16,5 @@ st.set_page_config(
 # source_code = HtmlFile.read() 
 # print(source_code)
 #components.html(source_code, height = 700, width=800,scrolling =True)
-components.iframe("https://dionysisk.eu/RSA/report.html",height = 700, width=800,scrolling =True)
+components.iframe("https://dionysisk.eu/RSA/report.html",height = '70%', width="80%",scrolling =True)
 
