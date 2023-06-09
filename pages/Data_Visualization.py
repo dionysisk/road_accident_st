aac0697f2acb591e3417c335fb14e66f383dd9af
Dimitris Patros <50484525+DimitrisPatros@users.noreply.github.com>
2023-06-09 13:00:58 +0300

    Update Data_Visualization.py

--- pages/Data_Visualization.py
+++ pages/Data_Visualization.py
@@ -18,20 +18,25 @@ st.set_page_config(
 #components.html(source_code, height = 700, width=800,scrolling =True)
 #components.iframe("https://dionysisk.eu/RSA/report.html",height = '70%', width="80%",scrolling =True)
 
+# components.html(
+# '''
+#  <iframe id="idIframe" onload="iframeLoaded()" frameborder="0" src="https://dionysisk.eu/RSA/report.html" height="100%" width="100%" scrolling="no"></iframe>
+#  ''')
+
+
+# <script type="text/javascript">
+#                       function iframeLoaded() {
+#                           var iFrameID = document.getElementById('idIframe');
+#                           if(iFrameID) {
+#                                 // here you can make the height, I delete it first, then I make it again
+#                                 iFrameID.height = "";
+#                                 iFrameID.height = iFrameID.contentWindow.document.body.scrollHeight + "px";
+#                           }   
+#                       }
+#                     </script> 
 components.html(
 '''
- <iframe id="idIframe" onload="iframeLoaded()" frameborder="0" src="https://dionysisk.eu/RSA/report.html" height="100%" width="100%" scrolling="no"></iframe>
+<iframe src="https://dionysisk.eu/RSA/report.html" width="100vw" height="50vh">
+  <p>Hi SOF</p>
+</iframe>
  ''')
-
-
-<script type="text/javascript">
-                      function iframeLoaded() {
-                          var iFrameID = document.getElementById('idIframe');
-                          if(iFrameID) {
-                                // here you can make the height, I delete it first, then I make it again
-                                iFrameID.height = "";
-                                iFrameID.height = iFrameID.contentWindow.document.body.scrollHeight + "px";
-                          }   
-                      }
-                    </script> 
-
