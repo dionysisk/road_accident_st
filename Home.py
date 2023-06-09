58546814fe9d784df18395997d9b8368195be351
Dimitris Patros <50484525+DimitrisPatros@users.noreply.github.com>
2023-06-09 14:25:37 +0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -12,12 +12,6 @@ st.set_page_config(
 """
 from PIL import Image
 st.image("https://upload.wikimedia.org/wikipedia/commons/2/2f/Multi_vehicle_accident_-_M4_Motorway%2C_Sydney%2C_NSW_%288076208846%29.jpg",
-            width=700 # Manually Adjust the width of the image as per requirement
+            width=100% # Manually Adjust the width of the image as per requirement
         )
-
-st.write("# Welcome to Streamlit! 👋")
-
-#st.sidebar.success("Select a demo above.")
-
-
 
