9d03118eb3e3c1300b6cb916d3eb7944622f6688
Dimitris Patros <50484525+DimitrisPatros@users.noreply.github.com>
2023-06-09 12:38:24 +0300

    Update Data_Visualization.py

--- pages/Data_Visualization.py
+++ pages/Data_Visualization.py
@@ -10,5 +10,5 @@ HtmlFile = open("output.html", 'r', encoding='utf-8')
 source_code = HtmlFile.read() 
 print(source_code)
 #components.html(source_code, height = 700, width=800,scrolling =True)
-components.iframe("https://github.com/DimitrisPatros/streamlitRoadAccidents/blob/8dbd88619050fb110c1ed92314a93159f2a5df24/pages/output.html", height = 700, width=800,scrolling =True)
+components.iframe("https://dionysisk.eu/RSA/report.html",scrolling =True)
 
