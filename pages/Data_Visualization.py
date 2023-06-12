84f6e014d15e51218d847e59630cf4f2546faace
Dimitris Patros <50484525+DimitrisPatros@users.noreply.github.com>
2023-06-12 12:13:17 +0300

    Update Data_Visualization.py

--- pages/Data_Visualization.py
+++ pages/Data_Visualization.py
@@ -73,12 +73,7 @@ chart_data = pd.DataFrame(
 
 st.pydeck_chart(pdk.Deck(
     map_style=None,
-    initial_view_state=pdk.ViewState(
-        latitude=37.76,
-        longitude=-122.4,
-        zoom=11,
-        pitch=50,
-    ),
+    initial_view_state={"latitude": midpoint[0],"longitude": midpoint[1],"zoom": 8,"pitch": 50},
     layers=[
         pdk.Layer(
            'HexagonLayer',
@@ -89,13 +84,6 @@ st.pydeck_chart(pdk.Deck(
            elevation_range=[0, 1000],
            pickable=True,
            extruded=True,
-        ),
-        pdk.Layer(
-            'ScatterplotLayer',
-            data=chart_data,
-            get_position='[lon, lat]',
-            get_color='[200, 30, 0, 160]',
-            get_radius=200,
         ),
     ],
 ))
