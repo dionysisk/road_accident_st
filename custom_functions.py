9239e3b1ee815e83172d7371954af4384232f9fa
Dimitris Patros <50484525+DimitrisPatros@users.noreply.github.com>
2023-06-12 16:27:41 +0300

    Delete custom_functions.py

--- custom_functions.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import streamlit as st
-
-def read_uploaded_file(uploaded_file):
-  if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file,low_memory=False).sample(n=100000)
-    return df
